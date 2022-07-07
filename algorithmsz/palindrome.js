ad496e1463094dabccd5cd7b07b548619c022b09
James Gitchell <jamesgitchell.dev@gmail.com>
2022-07-07 01:38:23 -0400

    init commit, need to work on 1st alg.

---
+++ algorithmsz/palindrome.js
@@ -0,0 +1,25 @@
+//palindrome algorithm
+
+let letters = []
+
+let word = "racecar"
+let rword=""
+
+for(let i = 0; i < word.length; i++ ){
+    let letter = word[i];
+    letters.push(letter);
+    console.log(letters)
+}
+for(let i = 0; i < letters.length; i++){
+    rword += letters.pop()
+    console.log(rword)
+}
+if ( word === rword ){
+    console.log("it is a palendrome")
+} else {
+    console.log("it is not a palindrome")
+    
+}
+
+console.log(word)
+console.log(rword)
