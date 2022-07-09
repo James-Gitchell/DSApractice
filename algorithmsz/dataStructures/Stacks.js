ca2533529e5b2ffe8ab2e76b080c485859ab912d
James Gitchell <jamesgitchell.dev@gmail.com>
2022-07-09 00:41:53 -0400

    added Sets to DS

---
+++ algorithmsz/dataStructures/Stacks.js
@@ -0,0 +1,36 @@
+/* Stacks */
+
+// functions : push, pop, peek, length
+
+
+let Stack = function() {
+    this.count = 0;
+    this.storage = {};
+}
+
+//Adds a value onto the end of the stack
+this.push = function(value) {
+    this.storage[this.count] = value;
+    this.count++
+}
+
+// Removes and returns the value at the end oof the stack
+this.pop = function(){
+    if (this.count === 0) {
+        return undefined;
+    }
+
+    this.count--;
+    let result = this.storage[this.count]
+    delete this.storage[this.count]
+    return result;
+}
+this.size = function() {
+    return this.count
+}
+
+// Returns the value at the end of the stack
+this.peek = function() {
+    return this.storage[this.count-1];
+
+}
